814c43d82dce1288263d413bb4b2bfcabc42b461
Marco <marco.bustillo@directasia.com>
2022-09-12 16:42:51 +0800

    updates typescript definition

--- client/src/API.ts
+++ client/src/API.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from 'axios'
 
-const baseUrl: string = process.env.REACT_APP_SERVER_URL
+const baseUrl: string | undefined = process.env.REACT_APP_SERVER_URL
 
 export const getTodos = async (): Promise<AxiosResponse<ApiDataType>> => {
   try {
