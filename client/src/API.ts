0f15790e3c9fd3605008259ded3eb99adc86616b
Marco <marco.bustillo@directasia.com>
2022-09-12 16:40:03 +0800

    updates

--- client/src/API.ts
+++ client/src/API.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from 'axios'
 
-const baseUrl: string = 'http://localhost:4000'
+const baseUrl: string = process.env.REACT_APP_SERVER_URL
 
 export const getTodos = async (): Promise<AxiosResponse<ApiDataType>> => {
   try {
